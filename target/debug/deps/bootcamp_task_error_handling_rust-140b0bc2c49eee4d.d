57fca0e2c360cbc1248e49c81e850a37d7b581fc
paradogs-7 <vnyz2002@gmail.com>
2025-04-28 11:33:46 +0300

    First commit

---
+++ target/debug/deps/bootcamp_task_error_handling_rust-140b0bc2c49eee4d.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-task-error-handling-rust/target/debug/deps/libbootcamp_task_error_handling_rust-140b0bc2c49eee4d.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-task-error-handling-rust/target/debug/deps/bootcamp_task_error_handling_rust-140b0bc2c49eee4d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bootcamp_task_error_handling_rust-be7c77c2b282aa27.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-task-error-handling-rust/target/debug/deps/libbootcamp_task_error_handling_rust-be7c77c2b282aa27.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-task-error-handling-rust/target/debug/deps/bootcamp_task_error_handling_rust-be7c77c2b282aa27.d: src/main.rs
+
+src/main.rs:
